c61fb705807fd57b7c865e9bb1403fc781f05211
Alexey Latushkin <alexeilatushkin@yandex.ru>
2026-05-25 18:14:53 +0300

    version with a problem

---
+++ src/test/java/ru/solnyshko/SelenideTest.java
@@ -0,0 +1,42 @@
+package ru.solnyshko;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+
+
+    @Test
+    public void testGitIgnoreSearch(){
+        open("https://github.com/");
+        $(".header-search-button").click();
+        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+        $("#query-builder-test").submit();
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(withText(".gitignore")).should(Condition.exist);
+    }
+
+    @Test
+    public void testGitIgnoreSearch1(){
+        open("https://github.com/");
+        $(".header-search-button").click();
+        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+        $("#query-builder-test").submit();
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(withText(".gitignor1")).should(Condition.exist);
+    }
+
+    @Test
+    public void testGitIgnoreSearch2(){
+        open("https://github.com/");
+        $(".header-search-button").click();
+        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+        $("#query-builder-test").submit();
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(withText(".gitignor")).should(Condition.exist);
+    }
+}
